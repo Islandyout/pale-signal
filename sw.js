cafac6cd1b58f69f4150c5c65e048fab5911494f
Rastacia Fowlin <108405450+Islandyout@users.noreply.github.com>
2026-08-28 15:13:06 -0500

    Bump PWA cache for ship polish pass

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pale-signal-v6';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./live/live.js','./live/mobile-quality.js','./live/tethys-kestra-v2.js','./live/tethys-kestra-v3.js','./prototype/pale-signal.html.html'];
+const CACHE='pale-signal-v7';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./live/live.js','./live/mobile-quality.js','./live/tethys-kestra-v2.js','./live/tethys-kestra-v3.js','./live/ship-polish-v4.js','./prototype/pale-signal.html.html'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',event=>{
