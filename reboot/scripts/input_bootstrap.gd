b9ea1d61db282b7396095dbf7273f7338673e68e
Rastacia Fowlin <108405450+Islandyout@users.noreply.github.com>
2026-08-31 16:25:09 -0500

    Route F4 to cinematic skip only

--- reboot/scripts/input_bootstrap.gd
+++ reboot/scripts/input_bootstrap.gd
@@ -23,7 +23,7 @@ static func ensure_actions() -> void:
 	_bind_key("fabricator", KEY_U)
 	_bind_key("tutorial", KEY_F2)
 	_bind_key("tutorial_reset", KEY_F3)
-	_bind_key("tutorial_skip", KEY_F4)
+	_bind_key("cutscene_skip", KEY_F4)
 	_bind_key("cutscene_skip", KEY_ESCAPE)
 	_bind_mouse("scan", MOUSE_BUTTON_RIGHT)
 
